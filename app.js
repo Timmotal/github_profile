cd5914ef9dc7b6b6a091a6d6889b3a60ed322a70
move style to head <timmortal44@yahoo.com>
2022-11-03 23:36:04 +0100

    add github api

---
+++ app.js
@@ -0,0 +1,7 @@
+// Today is May 12th, 2 days to my 3 months of Programming from Feb 14, what have i achieved?1207 hours
+// const  APIURL = 'https://api.github.com/users/' ;
+//  he wants to get "repos" alongside the github profiles
+const  APIURL = "https://api.github.com/users/" ;
+
+
+
